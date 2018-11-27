34de32840266608ec258097fa51be865e9133c22
Subham Jain <Subham_Jain@syntelinc.com>
2018-11-27 09:57:35 +0000

    Babel ESlint config

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 module.exports = {
     extends: 'airbnb',
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+            modules: true
+        }
+    },
     rules: {}
 };
