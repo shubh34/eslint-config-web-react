98faddfe87b4f8491a20ca91aa007c6c7f7a7df7
Subham Jain <Subham_Jain@syntelinc.com>
2019-01-29 23:43:49 +0000

    eslint

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ module.exports = {
     },
     rules: {
         "react/jsx-filename-extension": 0,
+        'react/jsx-indent': [2, 'tab'],
+		'react/jsx-indent-props': [2, 'tab'],
         "no-shadow":0,
         'import/no-extraneous-dependencies': 0,
-        "indent": ["error", 4],
+        "indent": ["error", 2, "tab"],
         "no-trailing-spaces": "error",
         "eol-last": ["error", "always"]
+     
     }
 };
